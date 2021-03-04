8bf65ef8a6ee03a09c9d9316b4c73e6e6d6c74ff
andrew2004Z <zuikovazd@gmail.com>
2021-03-04 20:38:44 +0300

    Добавленны все функции шифрования

--- generate_hash.py
+++ generate_hash.py
@@ -28,3 +28,32 @@ class generate_hash:
     def sha512_hash(text):
         text1 = str.encode(text, encoding='utf-8')
         return hashlib.sha512(text1).hexdigest()
+
+    def blake2b_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.blake2b(text1).hexdigest()
+
+
+    def blake2s_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.blake2s(text1).hexdigest()
+
+
+    def sha3_224_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.sha3_224(text1).hexdigest()
+
+
+    def sha3_256_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.sha3_256(text1).hexdigest()
+
+
+    def sha3_384_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.sha3_384(text1).hexdigest()
+
+
+    def sha3_512_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.sha3_512(text1).hexdigest()
