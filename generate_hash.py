5ef98263c3fe1cf49837eb408f1000bc01038b41
andrew2004Z <zuikovazd@gmail.com>
2021-03-04 20:29:41 +0300

    Добавлено шифрование md5

--- generate_hash.py
+++ generate_hash.py
@@ -1,3 +1,8 @@
 import hashlib
 
 types_hash = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'blake2b', 'blake2s', 'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512']
+
+class generate_hash:
+    def md5_hash(text):
+        text1 = str.encode(text, encoding='utf-8')
+        return hashlib.md5(text1).hexdigest()
