c81f5cd40c3b531e370bd9640812ecc84f34bc5e
andrew2004Z <zuikovazd@gmail.com>
2021-03-04 20:40:08 +0300

    Добавленна функция для шиврования всеми типами

--- generate_hash.py
+++ generate_hash.py
@@ -57,3 +57,33 @@ class generate_hash:
     def sha3_512_hash(text):
         text1 = str.encode(text, encoding='utf-8')
         return hashlib.sha3_512(text1).hexdigest()
+
+    def more_hash(text):
+        sp_hash = []
+        for i in types_hash:
+            bar.next()
+            if i == 'md5':
+                sp_hash.append([generate_hash.md5_hash(text), i])
+            elif i == 'sha1':
+                sp_hash.append([generate_hash.sha1_hash(text), i])
+            elif i == 'sha224':
+                sp_hash.append([generate_hash.sha224_hash(text), i])
+            elif i == 'sha256':
+                sp_hash.append([generate_hash.sha256_hash(text), i])
+            elif i == 'sha384':
+                sp_hash.append([generate_hash.sha384_hash(text), i])
+            elif i == 'sha512':
+                sp_hash.append([generate_hash.sha512_hash(text), i])
+            elif i == 'blake2b':
+                sp_hash.append([generate_hash.blake2b_hash(text), i])
+            elif i == 'blake2s':
+                sp_hash.append([generate_hash.blake2s_hash(text), i])
+            elif i == 'sha3_224':
+                sp_hash.append([generate_hash.sha3_224_hash(text), i])
+            elif i == 'sha3_256':
+                sp_hash.append([generate_hash.sha3_256_hash(text), i])
+            elif i == 'sha3_384':
+                sp_hash.append([generate_hash.sha3_384_hash(text), i])
+            elif i == 'sha3_512':
+                sp_hash.append([generate_hash.sha3_512_hash(text), i])
+        return sp_hash
